e162a5113dd43e8547a3d3b46d369b7df9cdbdf8
Rahul Kumar <rahulkumar@Rahuls-MacBook-Air.local>
2022-07-04 17:38:23 +0530

    Removed copyright and last styling from button

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -10,24 +10,35 @@ const Footer: React.FC = () => {
 		<Flex height={"auto"} className="Footer" alignItems="center" justifyContent={"center"} direction="column">
 			<Flex height={"8vh"} width={'100%'} justifyContent="center">
 				<Flex justifyContent="space-around" width={"100%"}>
+
 					<Flex width={"12%"}></Flex>
-					<Flex justifyContent={"center"} width={"10%"} borderRadius={"0 0 6rem 6rem"}><Flex justifyContent={"center"} fontSize="1.3rem" borderRadius={"0 0 6rem 6rem"} height={"100%"} width={"100%"} bgColor={"white"}> <a href="\">Home</a> </Flex></Flex>
+
+					<Flex justifyContent={"center"} width={"10%"} borderRadius={"0 0 6rem 6rem"}><Flex justifyContent={"center"} height={"100%"} width={"100%"}> <a href="\">Home</a> </Flex></Flex>
+					{/* <Flex justifyContent={"center"} width={"10%"} borderRadius={"0 0 6rem 6rem"}><Flex justifyContent={"center"} fontSize="1.3rem" borderRadius={"0 0 6rem 6rem"} height={"100%"} width={"100%"} bgColor={"white"}> <a href="\">Home</a> </Flex></Flex> */}
+
 					<Flex width={"12%"}></Flex>
-					<Flex justifyContent={"center"}  width={"10%"}><Flex><a href="\about">About Us</a></Flex></Flex>
+
+					<Flex justifyContent={"center"} width={"10%"}><Flex><a href="\about">About Us</a></Flex></Flex>
+
 					<Flex width={"12%"}></Flex>
-					<Flex justifyContent={"center"}  width={"10%"}><Flex><a href="\location">Location</a></Flex></Flex>
+
+					<Flex justifyContent={"center"} width={"10%"}><Flex><a href="\location">Location</a></Flex></Flex>
+
 					<Flex width={"12%"}></Flex>
-					<Flex justifyContent={"center"}  width={"10%"}><Flex><a href="\contact">Contact Us</a></Flex></Flex>
+
+					<Flex justifyContent={"center"} width={"10%"}><Flex><a href="\contact">Contact Us</a></Flex></Flex>
+
 					<Flex width={"12%"}></Flex>
+
 				</Flex>
 			</Flex>
-			<Flex height={"5vh"} width={"100%"} justifyContent="space-around" alignItems={"center"}>
-				{/* <Flex fontSize={"2xl"}><FaLinkedin /></Flex>
-				<Flex fontSize={"2xl"}><FaGithub /></Flex> */}
+			{/* <Flex height={"5vh"} width={"100%"} justifyContent="space-around" alignItems={"center"}>
+				<Flex fontSize={"2xl"}><FaLinkedin /></Flex>
+				<Flex fontSize={"2xl"}><FaGithub /></Flex>
 				<Flex>All Rights Reserved &copy;Pensive2022</Flex>
-				{/* <Flex fontSize={"2xl"}><FaInstagram /></Flex>
-				<Flex fontSize={"2xl"}><FaFacebook /></Flex> */}
-			</Flex>
+				<Flex fontSize={"2xl"}><FaInstagram /></Flex>
+				<Flex fontSize={"2xl"}><FaFacebook /></Flex>
+			</Flex> */}
 		</Flex>
 	)
 }
