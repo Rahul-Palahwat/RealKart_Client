db3f018ad750849187dcf5391b1ef7813505a803
Kartik Setia <kartset10@gmail.com>
2022-07-05 11:56:50 +0530

    Created a Wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,37 @@
 // import AllProducts from './components/AllProducts/AllProducts';
 
-import { Routes, Route, BrowserRouter as Router } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 
 import Footer from './components/Footer/Footer';
 import Navbar from './components/Navbar/Navbar';
 import TotalProducts from './components/TotalProducts/TotalProducts';
 import AllProducts from './components/AllProducts/AllProducts';
+import { Box } from '@chakra-ui/react';
 
 function App() {
 	return (
-		<Router>
-			<div style={{ "backgroundColor": "white" }}>
-				<div>
-					<Navbar />
-				</div>
-				<Routes>
-					<Route path="/" element={<AllProducts/>}></Route>
-					<Route path="/all" element={<TotalProducts/>}></Route>
-				</Routes>
-				<Footer />
-			</div>
-		</Router>
+		<Wrapper>
+			<Routes>
+				<Route path="/" element={<AllProducts/>}></Route>
+				<Route path="/all" element={<TotalProducts/>}></Route>
+			</Routes>
+		</Wrapper>
   	);
+}
+
+
+const Wrapper = (props:any) => {
+	return (
+		<Box style={{ "backgroundColor": "white" }}>
+			<Navbar />
+			<Box>
+				{props.children}
+			</Box>
+			<Box>
+				<Footer />
+			</Box>
+		</Box>
+	)
 }
 
 export default App;
