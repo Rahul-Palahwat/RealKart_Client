9eafd1370acbfebeca2ac0808ce44fc74f2922b7
Kartik Setia <kartset10@gmail.com>
2022-07-05 12:11:14 +0530

    code cleaning

--- src/components/AllProducts/AllProducts.tsx
+++ src/components/AllProducts/AllProducts.tsx
@@ -1,69 +1,49 @@
 import { Flex } from '@chakra-ui/react'
 import React, { useState } from 'react'
-
 import { MdArrowBackIosNew, MdArrowForwardIos } from 'react-icons/md'
-
 import data from '../data.json'
 import Product from '../Product/Product'
-
 import './AllProducts.css'
 
+interface Data {
+    itemCode: number
+    category?: string
+    imgLink: string[]
+    title: string
+    location?: string
+    owner?: string
+    used?: string
+    newprice: number
+    description?: any
+}
+
 const AllProducts: React.FC = () => {
-
     const dataItems: number[] = [1, 2, 3];
-
-    interface Data {
-        itemCode: number
-        category?: string
-        imgLink: string[]
-        title: string
-        location?: string
-        owner?: string
-        used?: string
-        newprice: number
-        description?: any
-    }
-
     const [itemsPerPage, setItemsPerPage] = useState<number>(7);
-    console.log(setItemsPerPage);
-
     const [pageNumber, setPageNumber] = useState<number>(1);
-
     const indexOfLastItem: number = pageNumber * itemsPerPage;
-
     const indexofFirstItem: number = indexOfLastItem - itemsPerPage;
-
     const pageData: Data[] = data.slice(indexofFirstItem, indexOfLastItem);
+    return (<>
+        {dataItems.map((item) => (
+            <Flex m={2} mt={5} mb={5} className='AllProducts' direction="column" height={"auto"} borderRadius={8}>
+                <Flex alignItems={"center"} justifyContent="space-between" width={"auto"} height={"7vh"} m={1} p={2}>
+                    <Flex fontSize={"2xl"} fontWeight="bold" ml={4}>{item === 1 ? "All Products" : item === 2 ? "Best Selling" : "Most Wishlisted"}</Flex>
+                    <Flex pr={5}><a className='viewAll' href='/all'>VIEW ALL</a></Flex>
+                </Flex>
 
-    // console.log("Page data", { pageData });
-
-    
-
-
-
-    return (
-        <>
-            {dataItems.map((item) => (
-                <Flex m={2} mt={5} mb={5} className='AllProducts' direction="column" height={"auto"} borderRadius={8}>
-                    <Flex alignItems={"center"} justifyContent="space-between" width={"auto"} height={"7vh"} m={1} p={2}>
-                        <Flex fontSize={"2xl"} fontWeight="bold" ml={4}>{item === 1 ? "All Products" : item === 2 ? "Best Selling" : "Most Wishlisted"}</Flex>
-                        <Flex pr={5}><a className='viewAll' href='/all'>VIEW ALL</a></Flex>
+                <Flex height={"auto"} alignItems="center">
+                    <Flex width={"0%"} height={"100%"} className='left-right' display={pageNumber > 1 ? "" : "none"} fontSize="4xl">
+                        <MdArrowBackIosNew className='pos_left' onClick={() => setPageNumber(pageNumber - 1)} />
                     </Flex>
-
-                    <Flex height={"auto"} alignItems="center">
-                        <Flex width={"0%"} height={"100%"} className='left-right' display={pageNumber > 1 ? "" : "none"} fontSize="4xl">
-                            <MdArrowBackIosNew className='pos_left' onClick={() => setPageNumber(pageNumber - 1)} />
-                        </Flex>
-                        {pageData.map((dat) => (<Product title={dat.title} image={dat.imgLink} price={dat.newprice} />))}
-                        <Flex width={"0%"} height={"100"} className='left-right' display={pageNumber >= (data.length / itemsPerPage) ? "none" : ""} fontSize={"4xl"}>
-                            <MdArrowForwardIos className="pos_right"  onClick={() => setPageNumber(pageNumber + 1)}/>
-                        </Flex>
+                    {pageData.map((dat) => (<Product title={dat.title} image={dat.imgLink} price={dat.newprice} />))}
+                    <Flex width={"0%"} height={"100"} className='left-right' display={pageNumber >= (data.length / itemsPerPage) ? "none" : ""} fontSize={"4xl"}>
+                        <MdArrowForwardIos className="pos_right"  onClick={() => setPageNumber(pageNumber + 1)}/>
                     </Flex>
                 </Flex>
-            ))}
-        </>
-
-    )
+            </Flex>
+        ))}
+    </>)
 }
 
 export default AllProducts
