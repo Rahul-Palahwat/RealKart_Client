8d8bf4aefae75c296773c6744945115940e21a2c
Rahul Kumar <rahulkumar@Rahuls-MacBook-Air.local>
2022-07-06 11:17:37 +0530

    Fixed Uniform padding in Product component

--- src/components/Product/Product.tsx
+++ src/components/Product/Product.tsx
@@ -27,12 +27,12 @@ const Product: React.FC<Props> = (props) => {
                 <Flex direction={"column"} alignItems="center" className='add' height="100%" width="100%">
                     <Flex mt={2}>{newtitle}</Flex>
                     <Flex mt={2}><img src={image[0]} alt="Hello" style={{ "height": "20vh"}} /></Flex>
-                    <Flex justifyContent={"space-between"}>
+                    <Flex justifyContent={"space-between"} mt={2}>
                         <Flex mt={2} color="#4167B2" fontWeight={"bold"}>&#x20B9;{price.toLocaleString("en-IN")}</Flex>
                         <Flex mt={2} ml={4} textDecoration="line-through" fontSize={"0.8rem"} alignItems="flex-end">&#x20B9;{price}</Flex>
                     </Flex>
                     <Flex id="cart" width="80%" height={"100%"} alignItems={"flex-end"} mt={3}>
-                        <Flex mb={2} width="100%" justifyContent={"center"}>
+                        <Flex mb={3} width="100%" justifyContent={"center"}>
                             <Button className='items' width="80%" color={"white"} bgColor={"#47B5FF"} fontSize="0.8rem" height={"1.8rem"}>Add to Cart</Button>
                         </Flex>
                     </Flex>
