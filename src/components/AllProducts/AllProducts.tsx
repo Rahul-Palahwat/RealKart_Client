6133303d03bdd21a851684b0682187c50d23ce7b
Rahul Kumar <rahulkumar@Rahuls-MacBook-Air.local>
2022-07-06 10:07:28 +0530

    Fixed arrow positions and changed class names as unique

--- src/components/AllProducts/AllProducts.tsx
+++ src/components/AllProducts/AllProducts.tsx
@@ -47,23 +47,23 @@ const AllProducts: React.FC = () => {
     }, [itemsInSlider])
 
     return (<>
-        <Flex m={2} mt={5} mb={5} className='AllProducts' direction="column" height={"auto"} borderRadius={8}>
+        <Flex m={2} mt={5} mb={5} className='AllProductstotal' direction="column" height={"auto"} borderRadius={8}>
             <Flex alignItems={"center"} justifyContent="space-between" width={"auto"} height={"7vh"} m={1} p={2}>
                 <Flex fontSize={"2xl"} fontWeight="bold" ml={4}>
                     All Products
                 </Flex>
                 <Flex pr={5} pt={"2rem"}>
-                    <a className='viewAll' href='/all'><Flex fontSize={"1.1rem"}>View All</Flex></a>
+                    <a className='viewAlltotal' href='/all'><Flex fontSize={"1.1rem"}>View All</Flex></a>
                 </Flex>
             </Flex>
             <Flex height={"auto"} alignItems="center">
                 {getAllProductsStatus === "FETCHING" ||  getAllProductsStatus === "NOT_STARTED"? <Flex alignItems={"center"} justifyContent={"center"} width={"100vw"} height={"46.7vh"}><Spinner size={"xl"} /></Flex> : <>
-                    <Flex width={"0%"} height={"100%"} className='left-right' display={pageNumber > 1 ? "" : "none"} fontSize="4xl">
-                        <MdArrowBackIosNew className='pos_left' onClick={() => setPageNumber(pageNumber - 1)} />
+                    <Flex width={"0%"} height={"100%"} display={pageNumber > 1 ? "" : "none"} fontSize="4xl">
+                        <MdArrowBackIosNew className='pos_lefttotal' onClick={() => setPageNumber(pageNumber - 1)} />
                     </Flex>
                     {items.map((dat: any) => (<Product title={dat.name} image={['https://imgs.search.brave.com/isdKYX7EEeNnP2ixz4e1HKCAMcviT21y9eh_DPmEuTE/rs:fit:711:225:1/g:ce/aHR0cHM6Ly90c2Ux/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5y/cklleG5HZEZQVDRj/M01IQXpvRmd3SGFF/OCZwaWQ9QXBp']} price={dat.sellingPrice} key={dat._id} />))}
-                    <Flex width={"0%"} height={"100"} className='left-right' display={pageNumber >= (dataAllProducts.total.length / itemsPerPage) ? "none" : ""} fontSize={"4xl"}>
-                        <MdArrowForwardIos className="pos_right" onClick={() => setPageNumber(pageNumber + 1)} />
+                    <Flex width={"0%"} height={"100"} display={pageNumber >= (dataAllProducts.total.length / itemsPerPage) ? "none" : ""} fontSize={"4xl"}>
+                        <MdArrowForwardIos className="pos_righttotal" onClick={() => setPageNumber(pageNumber + 1)} />
                     </Flex></>}
             </Flex>
         </Flex>
