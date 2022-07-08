a557401e011b1507e6d3ba8c1141a58a4ee56850
Rahul Kumar <rahulkumar@Rahuls-MacBook-Air.local>
2022-07-08 12:04:27 +0530

    Added google and facebook signup button

--- src/components/ModalSignUp/ModalSignUp.tsx
+++ src/components/ModalSignUp/ModalSignUp.tsx
@@ -6,6 +6,13 @@ import {
     ModalContent, ModalFooter, ModalHeader,
     ModalOverlay, useDisclosure
 } from '@chakra-ui/react'
+
+
+// import google png from assets folder 
+import Google from '../../assets/google.png'
+import facebook from '../../assets/facebook.png'
+
+// this is modal css file 
 import './ModalSignUp.css'
 
 interface ModalSignUpProps {
@@ -51,29 +58,52 @@ const ModalSignUp: React.FC<ModalSignUpProps> = ({ isOpen, onClose, onOpen }) =>
                                     </FormControl>
                                 </Flex>
                             </Flex>
-                            <Flex width={"50%"}>
-                                <FormControl>
-                                    <FormLabel mt="0.8rem" mb="0.8rem">E-mail</FormLabel>
-                                    <Input placeholder='Enter your E-mail address' type={"email"} />
-                                </FormControl>
-                            </Flex>
-                            <Flex width={"50%"}>
-                                <FormControl>
-                                    <FormLabel mt="0.8rem" mb="0.8rem">Contact No.</FormLabel>
-                                    <Input placeholder='Enter your phone number' type={"number"} />
-                                </FormControl>
-                            </Flex>
-                            <Flex width={"50%"}>
-                                <FormControl>
-                                    <FormLabel mt="0.8rem" mb="0.8rem">Password</FormLabel>
-                                    <Input placeholder='Enter your password' type={"password"} />
-                                </FormControl>
-                            </Flex>
-                            <Flex width={"50%"}>
-                                <FormControl>
-                                    <FormLabel mt="0.8rem" mb="0.8rem">Confirm Password</FormLabel>
-                                    <Input placeholder='Retype your password' type={"password"} />
-                                </FormControl>
+                            {/* After name  */}
+                            <Flex width={"100%"}>
+                                {/* Left flex  */}
+                                <Flex direction={"column"} width="50%">
+                                    <Flex width={"100%"}>
+                                        <FormControl>
+                                            <FormLabel mt="0.8rem" mb="0.8rem">E-mail</FormLabel>
+                                            <Input placeholder='Enter your E-mail address' type={"email"} />
+                                        </FormControl>
+                                    </Flex>
+                                    <Flex width={"100%"}>
+                                        <FormControl>
+                                            <FormLabel mt="0.8rem" mb="0.8rem">Contact No.</FormLabel>
+                                            <Input placeholder='Enter your phone number' type={"number"} />
+                                        </FormControl>
+                                    </Flex>
+                                    <Flex width={"100%"}>
+                                        <FormControl>
+                                            <FormLabel mt="0.8rem" mb="0.8rem">Password</FormLabel>
+                                            <Input placeholder='Enter your password' type={"password"} />
+                                        </FormControl>
+                                    </Flex>
+                                    <Flex width={"100%"}>
+                                        <FormControl>
+                                            <FormLabel mt="0.8rem" mb="0.8rem">Confirm Password</FormLabel>
+                                            <Input placeholder='Retype your password' type={"password"} />
+                                        </FormControl>
+                                    </Flex>
+                                </Flex>
+                                {/* middle line  */}
+                                <Flex className="center" width={"10%"} height={"50vh"} justifyContent="center">
+                                    <Flex className="line" />
+                                    <Flex className="or">OR</Flex>
+                                </Flex>
+                                {/* right flex  */}
+                                <Flex direction={"column"} alignItems="center" justifyContent={"center"} width={"40%"}>
+                                    <Flex color={"green"}>Why create an Account?</Flex>
+                                    <Flex className="googleButton google">
+                                        <img src={Google} alt="" className='icon' />
+                                        Google
+                                    </Flex>
+                                    <Flex className="googleButton facebook">
+                                        <img src={facebook} alt="" className='icon' />
+                                        Google
+                                    </Flex>
+                                </Flex>
                             </Flex>
                         </ModalBody>
 
