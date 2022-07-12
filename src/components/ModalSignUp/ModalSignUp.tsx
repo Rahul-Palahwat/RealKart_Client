12ea1ab56d4aae3834e2bd48ff4a06f144a7d8ee
Rahul Kumar <rahulkumar@Rahuls-MacBook-Air.local>
2022-07-12 14:59:32 +0530

    Changed the whole format of signup modal

--- src/components/ModalSignUp/ModalSignUp.tsx
+++ src/components/ModalSignUp/ModalSignUp.tsx
@@ -15,11 +15,14 @@ import { useAppDispatch, useAppSelector } from '../../redux'
 // import google png from assets folder 
 import Google from '../../assets/google.png'
 import facebook from '../../assets/facebook.png'
+import Email from '../../assets/Email.png'
 
 // this is modal css file 
 import './ModalSignUp.css'
 import { signInGoogle } from '../../redux/reducers/Login'
 import { text } from 'node:stream/consumers'
+
+// for form data handling 
 import { formReducer } from '../../utils/formReducer'
 import { formFields } from './data'
 
@@ -50,32 +53,14 @@ const initialState: InitialState = {
     confirmPassword: ''
 }
 
-// const formReducer = (state: any, action: any) => {
-//     switch (action.type) {
-//         case 'handleChange':
-//             return { ...state, [action.field]: action.payload }
-//         default:
-//             return state
-//     }
-// }
-
 const ModalSignUp: React.FC<ModalSignUpProps> = ({ isOpen, onClose, onOpen }) => {
 
     const [formState, dispatchChangeinFormState] = useReducer(formReducer, initialState);
 
-    const _changeInInput = (type:string, value:any) => {
-        console.log('In _change', {type, value})
-        dispatchChangeinFormState({type, value})
+    const _changeInInput = (type: string, value: any) => {
+        console.log('In _change', { type, value })
+        dispatchChangeinFormState({ type, value })
     }
-
-    // const handleChange = (e: any) => {
-    //     formDispatch({
-    //         type: 'handleChange',
-    //         field: e.target.name,
-    //         payload: e.target.value,
-    //     })
-    // }
-
     // for handling form submit 
     const handleSubmit = () => {
         console.log("hello")
@@ -88,7 +73,7 @@ const ModalSignUp: React.FC<ModalSignUpProps> = ({ isOpen, onClose, onOpen }) =>
         //logic to save user to the database
     }
 
-    console.log({formState})
+    console.log({ formState })
 
 
     const initialRef = React.useRef(null)
@@ -106,7 +91,7 @@ const ModalSignUp: React.FC<ModalSignUpProps> = ({ isOpen, onClose, onOpen }) =>
 
     return (
         <>
-            <Modal size={"3xl"}
+            <Modal size={"2xl"}
                 initialFocusRef={initialRef}
                 isOpen={isOpen}
                 onClose={onClose}
@@ -114,56 +99,102 @@ const ModalSignUp: React.FC<ModalSignUpProps> = ({ isOpen, onClose, onOpen }) =>
                 <ModalOverlay />
                 <ModalContent pl="2rem" pr={"2rem"}>
                     <Flex direction={"column"}>
-                        <form id='customer_form'>
-                            <ModalHeader ><Flex justifyContent="center">Create your account</Flex></ModalHeader>
-                            <ModalCloseButton />
-                            <ModalBody pb={6}>
-                                {/* After name  */}
-                                <Flex width={"100%"}>
-                                    {/* Left flex  */}
-                                    <Flex direction={"column"} width="50%">
-                                        {formFields.map((formField) => (
-                                            <Flex width={"100%"} key={formField.placeholder} mt="0.8rem" mb="0.8rem">
-                                                <FormControl>
-                                                    <FormLabel>{formField.label}</FormLabel>
-                                                    <Input required={formField.required} placeholder={formField.placeholder} name={formField.name} onChange={(e) => {console.log('in field ', {type:formField.name, value:e.target.value} );_changeInInput( formField.name ,e.target.value)}} />
-                                                </FormControl>
+                        <ModalHeader ><Flex justifyContent="center">Create your account</Flex></ModalHeader>
+                        <ModalCloseButton />
+                        <ModalBody pb={0}>
+                            <Flex width={"100%"} direction="column" alignItems={"center"} justifyContent="center" >
+                                {/* Left flex  */}
+                                <Flex direction={"column"} width="90%">
+                                    <form id='customer_form' onSubmit={(e) => { e.preventDefault(); handleSubmit() }}>
+
+                                        <Flex justifyContent={"center"} width="100%">
+                                            <Flex justifyContent={"space-between"} width={"50%"}>
+                                                {formFields.map((formField, idx) => {
+                                                    if (idx < 2) {
+                                                        return (
+                                                            <Flex width={"48%"} key={formField.placeholder}>
+                                                                <FormControl>
+                                                                    {/* <FormLabel mt="0.8rem" mb="0.8rem">{formField.label}</FormLabel> */}
+                                                                    <Input width={"100%"} fontSize={"0.7rem"} required={formField.required} placeholder={formField.placeholder} name={formField.name} onChange={(e) => { console.log('in field ', { type: formField.name, value: e.target.value }); _changeInInput(formField.name, e.target.value) }} />
+                                                                </FormControl>
+                                                            </Flex>
+                                                        )
+                                                    }
+                                                })}
                                             </Flex>
-                                        ))}
-                                        
-                                    </Flex>
-
-
-                                    {/* middle line  */}
-                                    {/* <Flex className="center" width={"10%"} height={"50vh"} justifyContent="center">
-                                        <Flex className="line" />
-                                        <Flex className="or">OR</Flex>
-                                    </Flex> */}
-                                    {/* right flex  */}
-                                    {/* <Flex direction={"column"} alignItems="center" justifyContent={"center"} width={"40%"}>
-                                        <Flex color={"green"}>Why create an Account?</Flex>
-                                        <Flex className="googleButton google" onClick={googleLogin}>
-                                            <img src={Google} alt="" className='icon' />
-                                            Google
                                         </Flex>
-                                        <Flex className="googleButton facebook">
-                                            <img src={facebook} alt="" className='icon' />
-                                            Facebook
+                                        <Flex direction="column" width="100%" alignItems={"center"}>
+                                            {formFields.map((formField, idx) => {
+                                                if (idx === 2) {
+                                                    return (
+                                                        <Flex width={"60%"} key={formField.placeholder}>
+                                                            <Flex alignItems="center" direction="column" width="100%">
+                                                                {/* <FormLabel mt="0.8rem" mb={idx === formFields.length-1 ? "0rem" :"0.8rem"}>{formField.label}</FormLabel> */}
+                                                                <Input fontSize={"0.7rem"} mt="0.8rem" mb="0.8rem" type={formField.type} required={formField.required} placeholder={formField.placeholder} name={formField.name} onChange={(e) => { console.log('in field ', { type: formField.name, value: e.target.value }); _changeInInput(formField.name, e.target.value) }} />
+                                                            </Flex>
+                                                        </Flex>
+                                                    )
+                                                }
+                                            })}
                                         </Flex>
-                                    </Flex> */}
+                                        <Flex justifyContent={"center"} width="100%">
+                                            <Flex justifyContent={"space-between"} width={"70%"}>
+                                                {formFields.map((formField, idx) => {
+                                                    if (idx > 2) {
+                                                        return (
+                                                            <Flex width={"48%"} key={formField.placeholder}>
+                                                                <FormControl>
+                                                                    {/* <FormLabel mt="0.8rem" mb="0.8rem">{formField.label}</FormLabel> */}
+                                                                    <Input width={"100%"} fontSize={"0.7rem"} required={formField.required} placeholder={formField.placeholder} name={formField.name} onChange={(e) => { console.log('in field ', { type: formField.name, value: e.target.value }); _changeInInput(formField.name, e.target.value) }} />
+                                                                </FormControl>
+                                                            </Flex>
+                                                        )
+                                                    }
+                                                })}
+                                            </Flex>
+                                        </Flex>
 
-
-
+                                    </form>
                                 </Flex>
-                            </ModalBody>
 
-                            <ModalFooter>
-                                <Button type='submit' form='customer_form' colorScheme='blue' mr={3} onClick={() => handleSubmit()}>
+
+
+                                {/* middle line  */}
+                                <Flex className="center" width={"90%"} height={"10vh"} justifyContent="center" alignItems={"center"} mt="1rem">
+                                    <Flex className="line" />
+                                    <Flex className="or">OR</Flex>
+                                </Flex>
+                                {/* right flex  */}
+                                <Flex direction={"column"} alignItems="center" justifyContent={"center"} width={"100%"} mt="1rem">
+                                    {/* <Flex color={"green"} mb={'1.5rem'}>Why  an Account?</Flex> */}
+                                    <Flex justifyContent={"space-between"} width={"100%"}>
+                                    <Flex className="googleButton google" onClick={googleLogin}>
+                                        <img src={Google} alt="" className='icon' />
+                                        Google
+                                    </Flex>
+                                    <Flex className="googleButton facebook">
+                                        <img src={facebook} alt="" className='icon' />
+                                        Facebook
+                                    </Flex>
+                                    <Flex className="googleButton email" onClick={googleLogin}>
+                                        <img src={Email} alt="" className='icon' style={{"filter":"invert(100%)"}}/>
+                                        Email
+                                    </Flex>
+                                    </Flex>
+                                </Flex>
+
+
+
+                            </Flex>
+                        </ModalBody>
+                        <Flex pb={2} width={"96.5%"} justifyContent="flex-end">
+                            <ModalFooter p={0}>
+                                <Button type='submit' form='customer_form' colorScheme='blue' mr={3}>
                                     Submit
                                 </Button>
                                 <Button onClick={onClose}>Cancel</Button>
                             </ModalFooter>
-                        </form>
+                        </Flex>
                     </Flex>
                 </ModalContent>
             </Modal>
