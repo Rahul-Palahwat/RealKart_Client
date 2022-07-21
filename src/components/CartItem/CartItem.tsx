f66288f8de3aa7d44d2474072ab246174d1437f3
Rahul Kumar <rahulkumar@Rahuls-MacBook-Air.local>
2022-07-21 18:35:44 +0530

    More changes in style of cart

--- src/components/CartItem/CartItem.tsx
+++ src/components/CartItem/CartItem.tsx
@@ -4,34 +4,34 @@ import './CartItem.css'
 // for redux
 import { useAppDispatch, useAppSelector } from '../../redux'
 import { removeItemCart } from '../../redux/reducers/items'
-import {removeOneItemFromCart, removeAllItemFromCart , addToCart} from '../../redux/reducers/CartItems'
+import { removeOneItemFromCart, removeAllItemFromCart, addToCart } from '../../redux/reducers/CartItems'
 import { CloseIcon, WarningIcon } from '@chakra-ui/icons'
 import _ from 'lodash'
 interface Props {
     item: any
 }
-const CartItem: React.FC<Props> = ({item}) => {
+const CartItem: React.FC<Props> = ({ item }) => {
     const dispatch = useAppDispatch()
-    const {items} = useAppSelector((state) => state.cartItem);
+    const { items } = useAppSelector((state) => state.cartItem);
     // const {dataAllCartProducts} = useAppSelector((state) => state.items);
     const countOfItems = () => {
-        let index = _.findIndex(items , function(o) { return o.id === item._id});
-        if(index !== -1){
+        let index = _.findIndex(items, function (o) { return o.id === item._id });
+        if (index !== -1) {
             return items[index].count;
         }
-        return 0; 
+        return 0;
     }
-    const removeAll = (id:string,sellingPrice:number) => {
+    const removeAll = (id: string, sellingPrice: number) => {
         dispatch(removeItemCart(id));
-        dispatch(removeAllItemFromCart({id:id , price:sellingPrice}))
+        dispatch(removeAllItemFromCart({ id: id, price: sellingPrice }))
         // console.log("dataAllCartProducts",dataAllCartProducts)
     }
     const removeOne = (id: string, price: number) => {
-        let index = _.findIndex(items , function(o) { return o.id === id})
-        if(items[index].count === 1){
+        let index = _.findIndex(items, function (o) { return o.id === id })
+        if (items[index].count === 1) {
             dispatch(removeItemCart(id));
         }
-        dispatch(removeOneItemFromCart({id: id , price: price}))
+        dispatch(removeOneItemFromCart({ id: id, price: price }))
     }
     return (
         <>
@@ -46,31 +46,31 @@ const CartItem: React.FC<Props> = ({item}) => {
                     <Flex mt={"0.5rem"}>
                         <Flex color="#4167B2" fontWeight={"bold"}>&#x20B9;{item.sellingPrice}</Flex>
                         <Flex ml={2} textDecoration="line-through" fontSize={"0.8rem"} alignItems="center">&#x20B9;{item.sellingPrice}</Flex>
-                        
+
                     </Flex>
-                    
-                    <Flex mt={"0.5rem"} mb={"0.5rem"}>
-                        <Flex color={"#388F3D"}>Delivery by :</Flex>
-                    <Flex ml={1} fontSize="0.8rem" alignItems={"center"}>27 July</Flex>
-                    <Flex ml={"1rem"}>Qty:<Flex ml={"0.5rem"} alignItems={"center"} justifyContent="center">
-                    <Tooltip label='Remove' hasArrow arrowSize={10}>
-                        <Badge cursor={"pointer"} colorScheme='green' onClick={() => removeOne(item._id , item.sellingPrice)} >-</Badge>
-                        </Tooltip>
-                        <Badge cursor={"pointer"} colorScheme='green'>{countOfItems()}</Badge>
-                        <Tooltip label='Add' hasArrow arrowSize={10}>
-                        <Badge cursor={"pointer"} colorScheme='green' onClick={() => dispatch(addToCart({id: item._id , price: item.sellingPrice}))} >+</Badge>
-                        </Tooltip>
+
+                    <Flex mt={"0.5rem"}>
+                        <Flex color={"#388F3D"} alignItems="center">Delivery by :</Flex>
+                        <Flex ml={1} fontSize="0.8rem" alignItems={"center"}>27 July</Flex>
+                        <Flex ml={"1rem"} alignItems="center">Qty:<Flex ml={"0.5rem"} alignItems={"center"} justifyContent="center">
+                            <Tooltip label='Remove' hasArrow arrowSize={10}>
+                                <Flex ml={"1rem"} cursor={"pointer"} alignItems="center" mb="0.2rem" justifyContent={"center"} color="red" fontSize={"2rem"} onClick={() => removeOne(item._id, item.sellingPrice)} >-</Flex>
+                            </Tooltip>
+                            <Badge cursor={"pointer"} ml="0.3rem" variant="outline" alignItems={"center"} >{countOfItems()}</Badge>
+                            <Tooltip label='Add' hasArrow arrowSize={10}>
+                                <Flex cursor={"pointer"} alignItems="center" mb="0.2rem" justifyContent={"center"} color="green" fontSize={"1.5rem"} ml={"0.3rem"} onClick={() => dispatch(addToCart({ id: item._id, price: item.sellingPrice }))} >+</Flex>
+                            </Tooltip>
                         </Flex></Flex>
-                     </Flex>
+                    </Flex>
                 </Flex>
                 <Flex width={"10%"} justifyContent={"flex-end"} m={"0.2rem"}>
-                <Tooltip label='Remove Item' hasArrow arrowSize={10}>
-                    <IconButton onClick={() => removeAll(item._id , item.sellingPrice)} aria-label='Call Segun' size={"xs"} fontSize={"0.8rem"} icon={<WarningIcon />} />
+                    <Tooltip label='Remove Item' hasArrow arrowSize={10}>
+                        <IconButton onClick={() => removeAll(item._id, item.sellingPrice)} aria-label='Call Segun' size={"xs"} fontSize={"0.8rem"} icon={<WarningIcon />} />
                     </Tooltip>
-                <Tooltip label='Remove Item' hasArrow arrowSize={10}>
-                    <IconButton ml={3} onClick={() => removeAll(item._id , item.sellingPrice)} aria-label='Call Segun' size='xs' fontSize={"0.5rem"} icon={<CloseIcon />} />
+                    <Tooltip label='Remove Item' hasArrow arrowSize={10}>
+                        <IconButton ml={3} onClick={() => removeAll(item._id, item.sellingPrice)} aria-label='Call Segun' size='xs' fontSize={"0.5rem"} icon={<CloseIcon />} />
                     </Tooltip>
-                    </Flex>
+                </Flex>
             </Flex>
         </>
     )
