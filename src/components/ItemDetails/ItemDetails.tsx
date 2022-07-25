01e52cd89dc5ae6eb90a39c0d21711ee01b1c096
Rahul Kumar <rahulpalahwat@gmail.com>
2022-07-25 16:06:48 +0530

    Added ItemDetails component

---
+++ src/components/ItemDetails/ItemDetails.tsx
@@ -0,0 +1,13 @@
+import { Flex } from '@chakra-ui/react'
+import React from 'react'
+
+import './ItemDetails.css'
+const ItemDetails:React.FC = () => {
+  return (
+    <>
+        <Flex>Item details</Flex>
+    </>
+  )
+}
+
+export default ItemDetails
